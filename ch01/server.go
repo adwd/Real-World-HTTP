e9884dcc29c70201ecc44a87c9921b8b733fe6b6
adwd <adwd@users.noreply.github.com>
2017-09-17 17:16:56 +0900

    chapter 2

--- ch01/server.go
+++ ch01/server.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
+
+	"github.com/k0kubun/pp"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -17,8 +20,47 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
 }
 
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(string(dump))
+	http.Redirect(w, r, "/foo", 303)
+}
+
+func cookieHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Set-Cookie", "VISIT=TRUE")
+	if _, ok := r.Header["Cookie"]; ok {
+		fmt.Fprintf(w, "<html><body>2回目以降</body></html>\n")
+	} else {
+		fmt.Fprintf(w, "<html><body>初回訪問</body></html>\n")
+	}
+}
+
+func digestHandler(w http.ResponseWriter, r *http.Request) {
+	pp.Printf("URL: %s\n", r.URL.String())
+	pp.Printf("Query: %v\n", r.URL.Query())
+	pp.Printf("Proto: %s\n", r.Proto)
+	pp.Printf("Method: %s\n", r.Method)
+	pp.Printf("Header: %v\n", r.Header)
+	defer r.Body.Close()
+	body, _ := ioutil.ReadAll(r.Body)
+	fmt.Printf("--body--\n%s\n", string(body))
+	if _, ok := r.Header["Authorization"]; !ok {
+		w.Header().Add("WWW-Authenticate", `Digest realm="Secret Zone", nonce="TgLc25U2BQA=f510a2780473e18e658be702c2e67fe2b04afd", algorithm=MD5, qop="quth"`)
+		w.WriteHeader(http.StatusUnauthorized)
+	} else {
+		fmt.Fprintf(w, "<html><body>secret</body></html>")
+	}
+}
+
 func main() {
 	var httpServer http.Server
+	http.HandleFunc("/redirect", redirectHandler)
+	http.HandleFunc("/cookie", cookieHandler)
+	http.HandleFunc("/digest", digestHandler)
 	http.HandleFunc("/", handler)
 	log.Println("start http listening :18888")
 	httpServer.Addr = ":18888"
